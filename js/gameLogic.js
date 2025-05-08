1ffd1103ee064bf258a63891592710b31d323097
maybdedling <maybdedling@gmail.com>
2025-05-08 13:15:21 +0100

    added offscreen resource drain wooo
    githubbing is hard fr fr

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,28 +1,25 @@
 import { updateDisplay } from "./uiFuncs.js";
 
-// import { resourceDrain, gameoverCheck, restartGame, saveStats, gameoverScreen } from "./gameLogic.js";
+// import { resourceDrain, gameoverCheck, restartGame, saveStats, gameoverScreen, offscreenDrain } from "./gameLogic.js";
 //
 //game funcs
 //
 export function resourceDrain(speed){ 
-    function drainer() {
-        if (gameoverCheck()) return;
-        setTimeout(function (){
-            food-= Math.floor(Math.random()*10);
-            updateDisplay();
-        }, Math.random()*2000*speed);
-        
-        setTimeout(function (){
-            poke-= Math.floor(Math.random()*10);
-            updateDisplay();
-        }, Math.random()*2000*speed);
-        
-        setTimeout(function (){
-            talk-= Math.floor(Math.random()*10);
-            updateDisplay();
-        }, Math.random()*2000*speed);
-    }
-    resourceDrainID = setInterval(drainer, speed*1000);
+    if (gameoverCheck()) return;
+    setTimeout(function (){
+        food-= Math.floor(Math.random()*10);
+        updateDisplay();
+    }, Math.random()*2000*speed);
+    
+    setTimeout(function (){
+        poke-= Math.floor(Math.random()*10);
+        updateDisplay();
+    }, Math.random()*2000*speed);
+    
+    setTimeout(function (){
+        talk-= Math.floor(Math.random()*10);
+        updateDisplay();
+    }, Math.random()*2000*speed);
 }
 export function gameoverCheck() {
     if (food<=0) {
@@ -50,6 +47,7 @@ export function restartGame() {
     poke = 50;
     food = 50;
     talk = 50;
+    saveStats();
     updateDisplay();
     resourceDrain(speed);
 }
@@ -57,9 +55,21 @@ export function gameoverScreen(displayText) {
     alertPane.style.visibility = "visible";
     jeoverText.innerText = `${displayText}`;
 }
-
 export function saveStats() {
     localStorage.setItem("poke", poke);
     localStorage.setItem("food", food);
     localStorage.setItem("talk", talk);
+    lastSave = Date.now();
+    localStorage.setItem("lastSave", lastSave);
+
+}
+export function offscreenDrain(){
+    let drain = Math.floor(time / speed);
+    food = Math.max(0, food - drain * 5);
+    poke = Math.max(0, poke - drain * 5);
+    talk = Math.max(0, talk - drain * 5);
+    saveStats();
+    // alert(food + ', ' + poke + ', ' + talk + ', ' + time + ', ' + drain + ', ' + now + ', ' + lastSave);
+    updateDisplay();
+    gameoverCheck();
 }
